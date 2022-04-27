4845ad59fa42d7f1fd3b826838884d76ce1e4494
zeriaxdev <hedgegorg@gmail.com>
2022-04-27 23:53:53 +0300

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: process.env.PUBLIC_URL || "http://0.0.0.0",
       changeOrigin: true,
     })
   );
